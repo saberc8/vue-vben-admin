c96f8fcaf246e57a755a23861934fe39b6b1697b
saber <1558365539@qq.com>
2022-11-25 18:10:32 +0800

    feat: add scroll style

--- src/App.vue
+++ src/App.vue
@@ -14,3 +14,30 @@
   const locale = zhCN
   useTitle()
 </script>
+
+<style>
+  /* 滚动条 */
+  body *::-webkit-scrollbar {
+    width: 4px;
+    height: 5px;
+  }
+
+  body *::-webkit-scrollbar-track {
+    background: #fff;
+    border-radius: 2px;
+  }
+
+  body *::-webkit-scrollbar-thumb {
+    background: rgb(205, 206, 206);
+
+    border-radius: 10px;
+  }
+
+  body *::-webkit-scrollbar-thumb:hover {
+    background: #333;
+  }
+
+  body *::-webkit-scrollbar-corner {
+    background: #fff;
+  }
+</style>
