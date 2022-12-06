fc3fdc7754759e902d4fae67b94be1ed84eb8e41
saber <1558365539@qq.com>
2022-12-06 18:32:13 +0800

    feat: mock table

---
+++ src/components/ProTable/components/ProForm/index.vue
@@ -0,0 +1,15 @@
+<!--
+ * @Author: saberc8
+ * @Date: 2022-12-02 16:41:30
+ * @LastEditTime: 2022-12-06 17:07:42
+ * @LastEditors: yusenlin
+ * @Description: ProTable 的筛选部分
+ * @FilePath: \vben-thin-next-1122\src\components\ProTable\components\ProForm\index.vue
+-->
+<template>
+  <div>header</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="less" scoped></style>
--- src/views/table/tableExample2/index.vue
+++ src/views/table/tableExample2/index.vue
@@ -1,6 +1,32 @@
 <template>
-  <ProTable />
+  <ProTable :dataSource="dataSource" :columns="columns" />
 </template>
 <script lang="ts" setup>
   import ProTable from '@/components/ProTable/index.vue'
+  import { demoListApi } from '@/api/demo/table'
+  import { DemoListItem } from '@/api/demo/model/tableModel'
+  console.log(typeof demoListApi)
+  const columns = [
+    { type: 'seq', width: 50 },
+    { field: 'name', title: 'name' },
+    { field: 'name1', title: 'sex', showHeaderOverflow: true },
+    { field: 'name2', title: 'Address', showOverflow: true },
+  ]
+
+  const params: DemoListItem = {
+    id: '',
+    beginTime: '',
+    endTime: '',
+    address: '',
+    name: '1',
+    no: 0,
+    status: 1,
+  }
+  let dataSource: any = ref([])
+  const t = async (params) => {
+    const res = await demoListApi(params)
+    console.log(res)
+    dataSource.value = res.items
+  }
+  t(params)
 </script>
