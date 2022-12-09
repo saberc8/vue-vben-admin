ceb06896c760dfa849ec36bf7e00cda03997e538
saber <1558365539@qq.com>
2022-12-09 18:29:46 +0800

    feat: table 样式

--- src/components/ProTable/index.vue
+++ src/components/ProTable/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <ProForm></ProForm>
-  <ProBody :columns="columns" :dataSource="dataSource"></ProBody>
+  <a-card><ProForm /></a-card>
+  <a-card class="margin-top-10"><ProBody :columns="columns" :dataSource="dataSource" /></a-card>
 </template>
 
 <script lang="ts" setup>
@@ -12,10 +12,16 @@
       default: {}
     }
     columns: Array<any>
+    searchForm: Array<any>
+    showForm: Boolean
   }>()
   console.log(props, 'pro-table')
   // console.log(props.dataSource, 'pro-table-dataSource')
   // console.log(props.columns, 'pro-table-columns')
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+  .margin-top-10 {
+    margin-top: 10px;
+  }
+</style>
